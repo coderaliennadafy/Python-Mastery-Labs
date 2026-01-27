90b165d1da8c7ad3715e5dd592c8a895d5c55281
coderaliennadafy <aennadafy@gmail.com>
2026-01-27 21:40:23 +0000

    forced real file content

---
+++ data_labs.py
@@ -0,0 +1,26 @@
+#ali
+programming_language = ['java' ,'javascript' ,'python' ,'c++']
+programming_language[0]= 'typescript'
+print(programming_language)
+programming_language[1] = 'php'
+print(programming_language)
+del programming_language[1]
+print(programming_language)
+programming_language = ['java' ,'javascript', ['python' ,'c++']]
+print(programming_language)
+print (programming_language[2])
+print(programming_language[2][1])
+print(programming_language[2][0])
+pro_prp = ['ali', 'hH72858', '26' ,'morocoo']
+name , id_card , age , nationality = pro_prp
+print(pro_prp)
+print(name)
+print(age)
+print(id_card)
+print(nationality)
+list_num = [1,2,3,4,5,6,7,8,9,10]
+print(list_num[0::5])
+print(list_num[0::2])
+print(list_num[0::3])
+print(list_num[0::4])
+print([ x for x in list_num if x % 2 == 0])
