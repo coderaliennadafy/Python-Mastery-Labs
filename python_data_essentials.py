6f42be6ddaf5d1b62c48cab0257e3c8e322bee0f
Ali Ennadafy <aennadafy@gmail.com>
2026-01-28 14:14:38 +0100

    Add various Python data manipulation examples

--- python_data_essentials.py
+++ python_data_essentials.py
@@ -40,3 +40,142 @@ print(f"Occurrences of 'php': {dev_skills.count('php')}") # Output: 2
 languages = ['java', 'javascript', 'python', 'c++']
 languages.sort(key=len)
 print(f"Sorted by length: {languages}")
+
+#reversing (reverse= True)
+programming_language = ["java", "javascript", "python", "c++","rast","c"]
+print(sorted(programming_language,reverse=True))
+#################################
+for char in "ali ennadafy":
+  print(char)
+###################################
+categoris = ['Fruit', 'Vegetable']
+names =["hamza", "ali", "fatimazahra", "oussama"]
+#####################################
+for categori in categoris:
+  for cat in names:
+    print(categori,cat)
+####################################"
+dev = ["java", "javascript", "python", "c++"]
+for devlopeur in dev:
+  if devlopeur == "python":
+    break;
+  print(devlopeur)
+  #########################################
+dev = ["dev", "nodejs", "nextjs","typescript"]
+for item in dev:
+  if item == "nextjs":
+    continue
+  print(item)
+######################################
+words = ['sky', 'apple', 'rhythm', 'fly', 'orange']
+
+for word in words:
+    for letter in word:
+        if letter.lower() in 'aeiou':
+            print(f"'{word}' contains the vowel '{letter}'")
+            break
+    else:
+        print(f"'{word}' has no vowels")
+##################################################
+for num in range(5):
+  print(num)
+###########################################
+for num in range(1,5):
+  print(num)
+#############################################
+for num in range(2,12,2):
+  print(num)
+#########################################
+for num in range(3):
+  pass
+print(num)
+############################################
+numbers = (1,2,3,4,5)
+print(numbers[2])
+############################################
+name = "jessica"
+print(tuple(name))
+print(list(name))
+print(set(name))
+#############################################
+number = list(range(2,11,2))
+print(number)
+##########################################
+name = ["lock", "weak","father", "sister"]
+print(list(enumerate(name)))
+#######################################
+even_numbers = [num for num in range(21) if num % 2 == 0]
+print(even_numbers)
+#####################################
+name = ["ali", "oussama", "fatima", "yahya"]
+for names in name:
+  print(names)
+######################################
+name = ["ahmed","ali","oussama","aymen"]
+
+index = 0
+
+for names in name:
+  print(f"index {index} :{names}")
+  index += 1
+###################################
+number = [1,2,3,4,5,6]
+
+result = [(number ,"even") if number % 2 == 0 else (number , "odd") for number in number]
+print(result)
+#####################################
+words = ["hmed", "lamin", "full", "the", "life", "inside"]
+
+def is_long(words):
+  return len(words) > 3
+
+res = list(filter(is_long,words))
+print(res)
+#############################
+celsius = [0, 10, 20, 30, 40]
+
+def is_celsius(celsius):
+  return (celsius * 9/5) + 32
+
+res = list(map(is_celsius,celsius))
+print(res)
+######################################
+number = [29,39,938,93,48,92]
+res = sum(number)
+print(res)
+
+number = [29,39,938,93,48,92]
+res = sum(number,10)
+print(res)
+
+number = [29,11, 12,14,93,957]
+res = sum(number, start=2)
+print(res)
+#####################################
+numbers =[19,3,9,18,26,86,20,199]
+numbers.sort()
+print(numbers)
+
+numbers.reverse()
+print(numbers)
+
+numbers =[19,3,9,18,26,86,20,199]
+print(numbers.index(20))
+
+names = ["ali", "oussama", "fatima", "yahya"]
+names.index("yahya")
+
+numbers.pop()
+print(numbers)
+###########################################
+developers = ['Naomi', 'Dario', 'Jessica', 'Tom']
+ids = [1, 2, 3, 4]
+
+for name, id in zip(developers, ids):
+    print(f'Name: {name}')
+    print(f'ID: {id}')
+###########################################
+numbers = [1, 2, 3, 4, 5]
+
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+print(even_numbers)  # [2, 4]
